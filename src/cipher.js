6d79ed592d13d713b4168047664d52dea32796ab
Sekai Strawberry <fran.castrobar@gmail.com>
2021-02-23 01:53:55 -0300

    funcional

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,49 @@
 const cipher = {
-  // ...
-};
+  encode: (text, code) => {
+    let asciiConvert;
+    for (let i = 0; i < text.length; i++) {
+      let ascii = text.charCodeAt(i);
+      if (ascii >= 65 && ascii <= 90) {
+        // MAYÚSCULAS
+        let upperFormula = ((ascii - 65 + code) % 26 + 65);
+        resultEncode[i] = upperFormula;
+        console.log(upperFormula);
+      } else if (ascii >= 97 && ascii <= 122) {
+        // MINÚSCULAS
+        let lowerFormula = ((ascii - 97 + code) % 26 + 97);
+        resultEncode[i] = lowerFormula;
+      } else {
+        let asciiConvert = ascii;
+        resultEncode[i] = asciiConvert;
+      }
+    }
+    for (let j = 0; j < resultEncode.length; j++) {
+      asciiConvert = String.fromCharCode(resultEncode[j]);
+      resultEncode[j] = asciiConvert;
+    }
+    return resultEncode;
+  },
 
-export default cipher;
+  decode: (resultEncode, code) => {
+    for (let k = 0; k < resultEncode.length; k++) {
+      let asciiDec = resultEncode[k].charCodeAt();
+      if (asciiDec <= 91 && asciiDec >= 61) {
+        let formulaUpperDec = (((asciiDec + 65) - code) % 26 + 65);
+        resultDecode[k] = formulaUpperDec;
+      } else if (asciiDec <= 122 && asciiDec >= 95) {
+        let formulaLowerDec = (((asciiDec - 122) - code) % 26 + 122);
+        resultDecode[k] = formulaLowerDec;
+      } else {
+        let formulaAsciiDec = asciiDec;
+        resultDecode[k] = formulaAsciiDec;
+      }
+    }
+    for (let l = 0; l < resultDecode.length; l++) {
+      let asciiDecode = String.fromCharCode(resultDecode[l]);
+      resultDecode[l] = asciiDecode;
+    }
+    return resultDecode;
+  }
+  };
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import cipher from './cipher.js';
+let text = document.getElementById("textEncode");
+let userCode = document.getElementById("offset");
+let button = document.getElementById("encodeButton");
+let button2 = document.getElementById("decodeButton");
+let printResult = document.getElementById("result");
+let resultEncode = [];
+let resultDecode = [];
 
-console.log(cipher);
+button.addEventListener("click", () => {
+  let textValue = text.value;
+  let userCodeValue = (parseInt(userCode.value));
+  resultEncode.toString;
+  cipher.encode(textValue, userCodeValue);
+  printResult.innerHTML = resultEncode.join("");
+});
+
+button2.addEventListener("click", () => {
+  let userCodeValue = (parseInt(userCode.value));
+  resultEncode.toString;
+  cipher.decode(resultEncode, userCodeValue);
+  printResult.innerHTML = resultDecode.join("");
+});
